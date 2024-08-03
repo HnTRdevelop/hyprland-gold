4499fc4b2e836784d9e536ed267fd0e5512d4136
hntr <sergeisiviz@gmail.com>
2024-08-03 19:01:50 +0500

    update

--- .config/waybar/getplayed.py
+++ .config/waybar/getplayed.py
@@ -10,6 +10,8 @@ if status != "":
         p = i.find(" ", i.find(":"))
         param = i[i.find(":") + 1:p]
         while i[p] == " ":
+            if p == len(i) - 1:
+                break
             p += 1
         value = i[p:]
         metadata[param] = value
