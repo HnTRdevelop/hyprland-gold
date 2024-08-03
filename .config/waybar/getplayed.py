060dfe248e6fd1140d4b926febd7e885bbf3f36d
hntr <sergeisiviz@gmail.com>
2024-08-03 15:13:50 +0500

    init

---
+++ .config/waybar/getplayed.py
@@ -0,0 +1,19 @@
+from os import popen
+
+
+status = popen("playerctl status").read().strip().lower()
+if status != "":
+    metadata = dict()
+    for i in popen("playerctl metadata").read().split("\n"):
+        if i == "":
+            continue
+        p = i.find(" ", i.find(":"))
+        param = i[i.find(":") + 1:p]
+        while i[p] == " ":
+            p += 1
+        value = i[p:]
+        metadata[param] = value
+
+    print(f"{"" if status == "playing" else ""} {metadata["artist"]}: {metadata["album"]} - {metadata["title"]}")
+else:
+    print("No player")
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+yay -S cava dunst hyprland hyprpaper hyprshot kitty waybar copyq nemo gnome-calculator gnome-calendar polkit-gnome pavucontrol chromium ponymix xdg-desktop-portal-hyprland wofi
+mv ./.config/ ~/.config/
