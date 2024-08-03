d30e8177017741579c4ad434c3254e3a914c4277
hntr <sergeisiviz@gmail.com>
2024-08-03 15:39:27 +0500

    add install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+dotfiles=`pwd`
+
+echo "Installing dependities with yay..."
 yay -S cava dunst hyprland hyprpaper hyprshot kitty waybar copyq nemo gnome-calculator gnome-calendar polkit-gnome pavucontrol chromium ponymix xdg-desktop-portal-hyprland wofi
-mv ./.config/ ~/.config/
+echo "Done!"
+echo 
+
+echo "Removing folders from .config..."
+cd ~/.config/
+echo "Folders to remove: `ls -1 $dotfiles/.config | xargs echo`"
+ls -1 $dotfiles/.config | xargs rm -rf 
+echo "Done!"
+echo
+
+echo "Moving configurations files into .config..."
+mv $dotfiles/.config/ ~/.config/
+echo "Installation done!"
